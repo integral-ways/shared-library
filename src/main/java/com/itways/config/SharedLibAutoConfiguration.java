6b8e49b697b8339475ae511ccba85dc6079855db
salahatwa <salahsayedatwa@gmail.com>
2025-08-10 08:25:38 +0300

    Adding Test utility

--- src/main/java/com/itways/config/SharedLibAutoConfiguration.java
+++ src/main/java/com/itways/config/SharedLibAutoConfiguration.java
@@ -1,11 +1,13 @@
 package com.itways.config;
 
-import com.itways.aspect.AspectApiResponse;
-import com.itways.exception.GlobalExceptionHandler;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import com.itways.aspect.AspectApiResponse;
+import com.itways.exception.GlobalExceptionHandler;
+
 @Configuration
-@Import({GlobalExceptionHandler.class, AspectApiResponse.class})
+@Import({ GlobalExceptionHandler.class, AspectApiResponse.class })
 public class SharedLibAutoConfiguration {
+
 }
