a049016447ddefe3c60c560a3d24684c2edc6444
salahatwa <salahsayedatwa@gmail.com>
2025-08-15 18:12:11 +0300

    Update common lib

--- src/main/java/com/itways/common/MessageConfig.java
+++ src/main/java/com/itways/common/MessageConfig.java
@@ -1,46 +1,21 @@
 package com.itways.common;
 
-import java.util.Locale;
-
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
-import org.springframework.web.servlet.LocaleResolver;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+import org.springframework.context.support.ResourceBundleMessageSource;
 
 @Configuration
-public class MessageConfig implements WebMvcConfigurer {
+public class MessageConfig {
 
 	@Bean
 	MessageSource messageSource() {
-		ReloadableResourceBundleMessageSource ms = new ReloadableResourceBundleMessageSource();
-		ms.setBasename("classpath:messages/messages"); // or "classpath:messages"
+		ResourceBundleMessageSource ms = new ResourceBundleMessageSource();
+		ms.setBasename("error-messages/message"); // or "classpath:messages"
 		ms.setDefaultEncoding("UTF-8");
 		ms.setUseCodeAsDefaultMessage(true);
-		ms.setCacheSeconds(10);
+//		ms.setCacheSeconds(10);
 		return ms;
 	}
 
-	@Bean
-	LocaleResolver localeResolver() {
-		SessionLocaleResolver slr = new SessionLocaleResolver();
-		slr.setDefaultLocale(Locale.ENGLISH);
-		return slr;
-	}
-
-	@Bean
-	LocaleChangeInterceptor localeChangeInterceptor() {
-		LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
-		lci.setParamName("lang");
-		return lci;
-	}
-
-	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-		registry.addInterceptor(localeChangeInterceptor());
-	}
 }
