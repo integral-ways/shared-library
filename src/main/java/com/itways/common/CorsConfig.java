109411525ad8c08bfddfbd609c7399338620b823
salahatwa <salahsayedatwa@gmail.com>
2025-08-30 14:54:57 +0300

    Fix core issue

--- src/main/java/com/itways/common/CorsConfig.java
+++ src/main/java/com/itways/common/CorsConfig.java
@@ -1,26 +1,26 @@
-package com.itways.common;
-
-//GatewayApplication.java
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        // allow Swagger-UI (gateway) or frontend domain
-                        .allowedOriginPatterns("*")   // ✅ use patterns when allowCredentials = true
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
-}
+//package com.itways.common;
+//
+////GatewayApplication.java
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        // allow Swagger-UI (gateway) or frontend domain
+//                        .allowedOriginPatterns("*")   // ✅ use patterns when allowCredentials = true
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true);
+//            }
+//        };
+//    }
+//}
