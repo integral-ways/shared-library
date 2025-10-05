b3bb8cc677beac7316d6b73d57ea8b073b9447cc
salahatwa <salahsayedatwa@gmail.com>
2025-10-05 07:16:05 +0300

    Adding ssl config for rest template

--- src/main/java/com/itways/common/CommonConfig.java
+++ src/main/java/com/itways/common/CommonConfig.java
@@ -1,12 +1,67 @@
 package com.itways.common;
 
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.SSLContext;
+
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+import org.apache.hc.client5.http.impl.io.PoolingHttpClientConnectionManagerBuilder;
+import org.apache.hc.client5.http.io.HttpClientConnectionManager;
+import org.apache.hc.client5.http.ssl.SSLConnectionSocketFactory;
+import org.apache.hc.core5.ssl.SSLContexts;
+import org.apache.hc.core5.ssl.TrustStrategy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.web.client.RestTemplate;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Configuration
 @ComponentScan(basePackages = "com.itways.common")
 @PropertySource("classpath:application-common.properties")
 public class CommonConfig {
 
+	@Bean
+	public RestTemplate restTemplate() {
+		System.setProperty("https.protocols", "TLSv1,TLSv1.1,TLSv1.2");
+		RestTemplate rest = new RestTemplate(clientHttpRequestFactory());
+
+		return rest;
+	}
+
+	private ClientHttpRequestFactory clientHttpRequestFactory() {
+
+		try {
+			TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+
+			SSLContext sslContext = SSLContexts.custom().setProtocol("TLSv1.2")
+					.loadTrustMaterial(null, acceptingTrustStrategy).build();
+
+			SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext);
+			// Allow TLSv1.2 protocol, use NoopHostnameVerifier to trust self-singed cert
+//			SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext,
+//			        new String[] { "TLSv1" , "TLSv1.1", "TLSv1.2"  }zz, null, new NoopHostnameVerifier());
+
+			HttpClientConnectionManager connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
+					.setSSLSocketFactory(csf).build();
+
+			CloseableHttpClient httpClient = HttpClients.custom().setConnectionManager(connectionManager).build();
+
+			HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(
+					httpClient);
+			//
+//			requestFactory.setHttpClient();
+			return requestFactory;
+		} catch (Exception e) {
+			e.printStackTrace();
+			log.error("Error SSL Config");
+			return null;
+		}
+	}
 }
