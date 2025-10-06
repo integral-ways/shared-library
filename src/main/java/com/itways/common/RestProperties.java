74fa78046c14b361b2c71c29b96f7fcb143f4fcd
salahatwa <salahsayedatwa@gmail.com>
2025-10-06 07:42:36 +0300

    Adding configurable endpoints

---
+++ src/main/java/com/itways/common/RestProperties.java
@@ -0,0 +1,25 @@
+package com.itways.common;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@ConfigurationProperties("itways.rest")
+@RefreshScope
+@Data
+public class RestProperties {
+	private long timeout;
+	private boolean offline;
+	private String offlineDirectory;
+	private Map<String, String> endpoints = new HashMap<String, String>();
+
+	public String getEndpoint(String key) {
+		return getEndpoints().get(key);
+	}
+}
